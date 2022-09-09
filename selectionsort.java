ca3da4d19b93d0c8cd42ee3e06af13bb51f585e1
Your Name <you@example.com>
2022-09-09 12:44:03 -0500

    h

--- selectionsort.java
+++ selectionsort.java
@@ -7,36 +7,37 @@
  * 
  */
 
-
 import java.util.Arrays;
-public class selectionsort{
+
+public class selectionsort {
     public static void main(String[] args) {
-        int arr[]={12,65,3,5,7,9,1};
+        int arr[] = { 12, 65, 3, 5, 7, 9, 1 };
         selectionsorts(arr);
         System.out.println(Arrays.toString(arr));
     }
 
-     static void selectionsorts(int[] arr) {
-        for(int i=0;i<arr.length;i++){
-            int last=arr.length-i-1;
-            int maxIndex=getMAx(arr,0,last);
+    static void selectionsorts(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMAx(arr, 0, last);
             swap(arr, maxIndex, last);
         }
     }
 
-    //swap functon
-    static void swap(int arr[],int first,int second){
-int temp=arr[first];
-arr[first]=arr[second];
-arr[second]=temp;
+    // swap functon
+    static void swap(int arr[], int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
 
     }
-// get the maximum value from the index 
-     static int getMAx(int[] arr, int start, int end) {
-        int max=start;
-        for(int i=start;i<end;i++){
-            if(arr[max]<arr[i]){
-                max=i;
+
+    // get the maximum value from the index
+    static int getMAx(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i < end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
             }
         }
         return max;
