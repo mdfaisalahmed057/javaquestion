10ebd64e5c42fd44550bd84b0e8879e2bde89fbb
Your Name <you@example.com>
2022-09-09 11:08:43 -0500

    first commit

---
+++ thg.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class thg {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter the year");
+      int year=sc.nextInt();
+      if(year%4)
+      sc.close();
+
+
+    }
+}
---
+++ threeg.java
@@ -0,0 +1,5 @@
+public  class threeg{
+    public static void main(String[] args) {
+        System.out.println("hekdd");
+    }
+}
