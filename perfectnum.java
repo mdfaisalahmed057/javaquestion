72f6518526f29b1a35424d6767771602d090ad21
Your Name <you@example.com>
2022-09-12 23:34:16 -0500

    rev

---
+++ perfectnum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class perfectnum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int sum=0;
+         int n=sc.nextInt();
+    for(int i=1;i<n;i++){
+        if(n%i==0){
+            sum=sum+i;
+         }
+    }
+    if(sum==n){
+        System.out.println("it is a perfect number");
+    }else{
+        System.out.println("not perfect");
+    }
+        
+    }
+    
+}
