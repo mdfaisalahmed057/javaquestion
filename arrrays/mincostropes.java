7c43bccee824d694f6492f305356cc42d84dbd8b
Your Name <mdfais2995@gmail.com>
2022-09-26 14:47:12 -0500

    not full complete

---
+++ arrrays/mincostropes.java
@@ -0,0 +1,30 @@
+package arrrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class mincostropes {
+    public static void main(String[] args) {
+        int arr[]={4,3,2,6};
+        
+      
+
+     
+        Arrays.sort(arr);// 2,3,4,6
+        int sum = 0;
+        int ans = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            sum = sum+arr[i] ;// 2+3=5//9
+            ans = sum + arr[i]; // 4+5==9
+
+            // sum = ans;//9
+
+        }
+        System.out.println(ans);
+        System.out.println(sum);
+
+
+    }
+
+}
