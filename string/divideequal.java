7a814bff93e9bac369e62ae092c9a69baa1af4ab
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-10-14 23:47:23 +0530

    divide the string

---
+++ string/divideequal.java
@@ -0,0 +1,36 @@
+public class divideequal {
+         public static void main(String[] args) {  
+              String str = "faisal";  
+              String vow="aeiou";
+             //Stores the length of the string  
+            int len = str.length();  
+            //n determines the variable that divide the string in 'n' equal parts  
+            int n = 2;  
+            int temp = 0, chars = len/n;  
+            //Stores the array of string  
+            String[] equalStr = new String [n];  
+            //Check whether a string can be divided into n equal parts  
+            if(len % n != 0) {  
+                System.out.println("Sorry this string cannot be divided into "+ n +" equal parts.");  
+            }  
+            else {  
+                for(int i = 0; i < len; i = i+chars) {  
+                    for(int j=0;j<vow.length();j++){
+
+                     //Dividing string in n equal part using substring()  
+                    String part = str.substring(i, i+chars);  
+                    if(part.charAt(i)==vow.charAt(j))
+                     equalStr[temp] = part;  
+                    temp++;  
+                    
+                }  
+            }
+        System.out.println(n + " equal parts of given string are ");  
+                for(int i = 0; i < equalStr.length; i++) {  
+                    System.out.println(equalStr[i]);  
+                    }  
+                }  
+            }  
+    }  
+    
+
