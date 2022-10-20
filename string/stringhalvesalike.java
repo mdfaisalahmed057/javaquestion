c9abc11f29c52da733671c501a2f1be727334d43
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-10-20 10:24:02 +0530

    rvst string

---
+++ string/stringhalvesalike.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class stringhalvesalike{
+    public static void main(String[] args) {
+        String str="fsal";
+        System.out.println(halvesAreAlike(str));
+        
+    }
+    public static boolean halvesAreAlike(String s) {
+        HashSet<Character> set = new HashSet<>((Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O','U')));
+     
+        int left =0;
+        int right = 0;
+        for(int i=0;i<s.length()/2;i++){
+             if(set.contains(s.charAt(i))){
+                left++;
+            }
+            if(set.contains(s.charAt(s.length()-1-i))){
+                right++;
+            }
+        
+    }
+        return left == right;
+    }
+}
