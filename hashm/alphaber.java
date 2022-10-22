bf9c0bcde0db5725284de4657b0c9805050bac7d
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-10-22 20:05:36 +0530

    map

---
+++ hashm/alphaber.java
@@ -0,0 +1,27 @@
+// //  import java.util.*;
+// public class alphaber {
+//     public static void main(String[] args) {
+//         int arr[]={1,2,3,4};
+//         System.out.println(two(arr));
+//      }
+// //      static boolean check(String sentence){
+// //         HashMap<Character,String> chasr=new HashMap<>();
+// //         for(int i=0;i<sentence.length();i++){
+// //             // chasr.put(sentence.charAt(i),0);
+// //                 }
+// //      }
+    
+// // }
+// static int two(int arr[]){
+//      int res=0;
+//     for(int i=0;i<arr.length-1;i++){
+//         res=res+arr[i]*arr[i+1];
+ 
+//     }
+//     return res;
+// }
+
+// }
+
+
+// Java code to illustrate the keySet() method
