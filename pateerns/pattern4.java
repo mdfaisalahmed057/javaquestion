d13f3135748c72a0523961dfec89a66dc790991f
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-10-25 09:58:14 +0530

    some change

--- pateerns/pattern4.java
+++ pateerns/pattern4.java
@@ -3,8 +3,8 @@ public class pattern4 {
         name(4);
     }
     public static void name(int n) {
-        for(int i=0;i<n;i++){
-            for(int j=1;j<=i+1;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
                 System.out.print(" "+j);
             }
 System.out.println();
