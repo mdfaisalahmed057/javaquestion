e5d1603ace98de521e9c765568eb41793307add6
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-10-25 09:26:27 +0530

    validpalindrme

---
+++ string/validpalindrome.java
@@ -0,0 +1,23 @@
+import javax.swing.plaf.TextUI;
+
+public class validpalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+    public static boolean isPalindrome(String s) {
+        s=s.toLowerCase();
+             char start=s.charAt(i);
+            char end=s.charAt(s.length()-1);
+        
+            if(start!=end)
+                return false;
+            else
+                 return true;
+            } 
+        
+        return false;
+        
+
+}
+}
