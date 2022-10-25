d83fc21e5cdafb11b1ddf4a175d91f47bb41da76
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-10-25 10:26:13 +0530

    hrd

--- pateerns/pattern5.java
+++ pateerns/pattern5.java
@@ -1,11 +1,12 @@
 public class pattern5 {
     public static void main(String[] args) {
-        name(9);
+        name(3);
     }
     public static void name(int n) {
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-5;j--){
-                System.out.print("4");
+        for(int i=1;i<=2*n;i++){
+            int totalcolsrow=i>n?2*n-i:i;
+            for(int j=1;j<=totalcolsrow;j++){
+                System.out.print("*");
             }
             System.out.println();
         }
