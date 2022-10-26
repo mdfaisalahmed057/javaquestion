e4c15dfbfee022ddd72b86b6a3aadc5382b26308
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-10-26 12:42:45 +0530

    yt video

---
+++ string/anangram.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class anangram{
+    public static void main(String[] args) {
+        boolean isanagram=true;
+        String a="amb";
+        String b="baa";
+        int al[]=new int[256];
+        int bl[]=new int[256];
+        for(char c:a.toCharArray()){
+            int index=(int)c;
+            al[index]++;
+        }
+        for(char c:b.toCharArray()){
+            int index=(int)c;
+            bl[index]++;
+        }
+for(int i=0;i<256;i++){
+    if(al[i]!=bl[i]){
+        isanagram=false;
+        break;
+    }
+}
+if(isanagram){
+    System.out.println("yes");
+}else{
+        System.out.println("no");
+    }
+}
+ 
+    }
+    
