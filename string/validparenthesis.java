1e3d9893dae6497480d46f13054727bd9c983417
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-10-29 22:46:48 +0530

    valid parenthesis

---
+++ string/validparenthesis.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.*;
+public class validparenthesis {
+    public static void main(String[] args) {
+       String s = "()";
+System.out.println(isValid(s)
+);
+    }
+    public static  boolean isValid(String s) {
+Stack<Character>st=new Stack<Character>();
+         for(char c:s.toCharArray()){
+            if(c=='{' )
+            st.push('}');
+            else if(c=='[')
+            st.push(']');
+           else if(c=='(')
+            st.push(')');
+            else if(st.isEmpty()||st.pop()!=c)
+            return false;
+         }
+         return st.isEmpty();
+        
+    }
+
+    
+}
