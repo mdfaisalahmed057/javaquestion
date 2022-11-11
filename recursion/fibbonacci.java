c69717d4cd9c39775046520240937c7428ff4789
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-11-11 23:27:25 +0530

    fibb

---
+++ recursion/fibbonacci.java
@@ -0,0 +1,17 @@
+ 
+public class fibbonacci{
+    public static void main(String[] args) {
+        System.out.println(name(4));
+        
+    }
+    public static int name(int n) {
+        if(n<2){//4<2
+            return n ;
+        }else{
+            return name(n-1)+name(n-2);
+
+        }
+        
+    }
+    
+}
