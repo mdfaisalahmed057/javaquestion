d862febb1a00b4591e677549b28b485cb7371723
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-11-13 12:23:11 +0530

    bubble sort

---
+++ recursion/bubblesort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class bubblesort {
+    public static void main(String[] args) {
+        int arr[]={4,2,5,6,1};
+        int n=arr.length;
+        bubble(arr, n);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void bubble(int arr[],int n) {
+        if(n==1){
+            return;
+        }
+        int count=0;
+        for(int i=0;i<n-1;i++)
+            if (arr[i] > arr[i+1])
+            {
+                // swap arr[i], arr[i+1]
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                  count = count+1;
+            }
+
+            if(count==0)
+            return;
+            bubble(arr, n-1);
+           
+
+        }
+
+        
+    
+    
+}
