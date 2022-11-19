b224586c737be5c129f4d29ecabe5cf87b584174
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-11-19 23:28:00 +0530

    normal

---
+++ recursion/insertionsort.java
@@ -0,0 +1,20 @@
+public class insertionsort{
+    public static void main(String[] args) {
+        int arr[]={4,1,3,5,2};
+        int n=arr.length;
+        System.out.println(insert(arr, n-1));
+        
+    }
+    public static int insert(int arr[],int n) {
+
+if(n<=1)
+    return 1;
+
+return n;
+ 
+}
+
+}
+
+
+//geeksfor geeks question it is
