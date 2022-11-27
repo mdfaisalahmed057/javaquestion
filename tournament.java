c69d3d6fbe1d4b94805c5a12a328ac2499faa82d
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-11-27 23:19:22 +0530

    tournament

---
+++ tournament.java
@@ -0,0 +1,11 @@
+import javax.swing.JSpinner.NumberEditor;
+
+public class tournament {
+    public static void main(String[] args) {
+int n=10;
+System.out.println(numberOfMatches(n));
+    }
+    public static int numberOfMatches(int n) {
+     return n-1;
+    }
+}
