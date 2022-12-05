7d4b3ff685d58be670ae8b0b521179ae050bd85d
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-12-05 12:11:33 +0530

    using arraylist

--- recursion/subsetproblem/subsequence.java
+++ recursion/subsetproblem/subsequence.java
@@ -1,22 +1,36 @@
- 
+import java.util.ArrayList;
+
 public class subsequence {
     public static void main(String[] args) {
         String p="";
         String up="abc";
-        name(p, up);
+        System.out.println(array(p, up));
+      }
 
-     }
-    public static void name(String p,String up) {
+      //using only recursion
+    // public static void name(String p,String up) {
+    //     if(up.isEmpty()){
+    //         System.out.println(p);
+    //         return;
+    //     }
+    //     char ch=up.charAt(0);
+    //          name(p+ch, up.substring(1));
+
+    //          name(p, up.substring(1));      
+    // }
+
+      //using only recursion and arraylist
+
+    public static  ArrayList<String> array(String p,String up) {
         if(up.isEmpty()){
-            System.out.println(p);
-            return;
-        }
+            ArrayList<String>ans=new ArrayList<>();
+            ans.add(p);
+            return ans;
+          }
         char ch=up.charAt(0);
-             name(p+ch, up.substring(1));
-
-         name(p, up.substring(1));
-        
-        
-    }
-    
+            ArrayList<String> left= array(p+ch, up.substring(1) );
+           ArrayList<String> right= array(p, up.substring(1)); 
+           left.addAll(right);
+         return left;  
+    } 
 }
