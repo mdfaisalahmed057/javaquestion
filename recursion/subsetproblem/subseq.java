b0064f9bb8fdfd2a3c20db9a30beca0a28525733
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-12-23 10:06:12 +0530

    subseq

---
+++ recursion/subsetproblem/subseq.java
@@ -0,0 +1,19 @@
+public class subseq {
+    public static void main(String[] args) {
+        String p="";
+        String up="abc";
+        name(p, up);
+    }
+    public static void name(String p,String up) {
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch=up.charAt(0);
+
+             name(p, up.substring(1));      
+             name(p+ch, up.substring(1));
+
+    }
+    
+}
