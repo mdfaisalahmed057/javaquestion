56c02e6f18a0afdc6811fd188807dc41641d9eb9
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-12-23 10:07:26 +0530

    normal code

---
+++ string/doubles.java
@@ -0,0 +1,12 @@
+ public class doubles{
+    public static void main(String[] args) {
+        int n=2;
+        String s="faisal";
+        System.out.println(n*2);
+
+        System.out.println(s);
+       
+        
+    }
+    
+ }
