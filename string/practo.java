621da5ff477ec51b0c31f49e75c8c8bdb64beadb
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-12-23 10:07:47 +0530

    wrong answer

---
+++ string/practo.java
@@ -0,0 +1,21 @@
+import java.util.*;
+ public class practo {
+    public static void main(String[] args) {
+        int x=1;
+        int y=1;
+        int z=1;
+        System.out.println(name(x, y, z));
+        
+    }
+    public static int name(int x ,int y,int z) {
+        int p=0;
+      
+
+        if(number1+number2<z){
+              p=number2/number1*number2/number1;
+
+        }
+        return p;
+    }
+    
+}
