b0ec0903e163374564c205fdf1cbfd714a1f6db2
mdfaisalahmed057 <mdfais2995@gmail.com>
2022-12-23 10:07:11 +0530

    stack chnages

---
+++ stack/stackmain.java
@@ -0,0 +1,17 @@
+// package stack;
+
+import java.util.Stack;
+
+public class stackmain {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.add(1);
+        st.add(2);
+        st.add(3);
+        st.add(4);
+        System.out.println(st);
+         for(int i=0;i<st.size();i++){
+         
+
+    }
+}
