72b69983174b19e46eb0bfdcb99989292e6e54c1
mdfaisalahmed057 <mdfais2995@gmail.com>
2023-01-07 12:25:15 +0530

    analog

--- string/anangram.java
+++ string/anangram.java
@@ -1,36 +1,34 @@
 import java.util.Arrays;
 
-public class anangram{
+public class anangram {
     public static void main(String[] args) {
-        String d="faisal";
+        String d = "faisal";
         System.out.println(d.chars());
-        
 
-        boolean isanagram=true;
-        String a="amb";
-        String b="baa";
-        int al[]=new int[256];
-        int bl[]=new int[256];
-        for(char c:a.toCharArray()){
-            int index=(int)c;
+        boolean isanagram = true;
+        String a = "amb";
+        String b = "baa";
+        int al[] = new int[256];
+        int bl[] = new int[256];
+        for (char c : a.toCharArray()) {
+            int index = (int) c;
             al[index]++;
         }
-        for(char c:b.toCharArray()){
-            int index=(int)c;
+        for (char c : b.toCharArray()) {
+            int index = (int) c;
             bl[index]++;
-         }
-for(int i=0;i<256;i++){
-    if(al[i]!=bl[i]){
-        isanagram=false;
-        break;
+        }
+        for (int i = 0; i < 256; i++) {
+            if (al[i] != bl[i]) {
+                isanagram = false;
+                break;
+            }
+        }
+        if (isanagram) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
     }
+
 }
-if(isanagram){
-    System.out.println("yes");
-}else{
-        System.out.println("no");
-    }
-}
- 
-    }
-    
