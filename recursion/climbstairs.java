f3fda1a9156226503fe23df44b0ac975653a7cd9
mdfaisalahmed057 <mdfais2995@gmail.com>
2023-02-01 23:57:17 +0530

    feb

--- recursion/climbstairs.java
+++ recursion/climbstairs.java
@@ -1,6 +1,7 @@
+import java.util.*;
 public class climbstairs {
     public static void main(String[] args) {
-        int n=5;
+        int n=4;
         System.out.println(stairs(n));
     }
     public static int stairs(int n) {
@@ -13,8 +14,24 @@ public class climbstairs {
         if(n==2){
         return 2;
          }
-         System.out.println(n);
-         return stairs(n-1)+stairs(n-2);//5//4
+
+       int ln=  stairs(n-1)+stairs(n-2);//5//4
+          return ln;
         
     }
+
+
+public static int climbStairs(int n) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+	if(map.containsKey(n)) return map.get(n);
+	
+	if(n == 1) return 1;
+	if(n == 2) return 2;
+	
+	int value = climbStairs(n-1) + climbStairs(n-2);
+	map.put(n, value);
+	
+	return value;
+}
 }
