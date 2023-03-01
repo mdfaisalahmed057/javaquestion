774b31d16088b9b3320da4995aa227038290fa59
mdfaisalahmed057 <mdfais2995@gmail.com>
2023-03-01 08:05:22 +0530

    ais

--- harshad.java
+++ harshad.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
 public class harshad {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int num=sc.nextInt();
-        boolean ans=harshad(num);
+       int num=20;
+        boolean ans=harshads(num);
         System.out.println(ans);
-    }
+     }
 
-    static boolean harshad(int num){
+    static boolean harshads(int num){
         int rev=0;
         int n=num;
         while(num>0){
@@ -23,5 +22,7 @@ public class harshad {
             return false;
 
         }
+        
     }
+    
 }
