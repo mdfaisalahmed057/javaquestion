aa33894ffb22a89e5a811d2b874277e06dd290bb
mdfaisalahmed057 <mdfais2995@gmail.com>
2023-03-06 08:41:38 +0530

    bubble sort

--- oops/classex.java
+++ oops/classex.java
@@ -1,7 +1,8 @@
- 
+import java.rmi.StubNotFoundException;
+
 // class Details{
 //     String name;
-//     String age;
+//     int age;
 
 // public void show(){
 //     System.out.println(this.name);
@@ -14,7 +15,7 @@
 //     public static void main(String[] args) {
 //         Details ob1=new Details();
 //         ob1.name="faisal";
-//         ob1.age="12";
+//         ob1.age=2;
 //         ob1.show();
 //     }
     
@@ -22,7 +23,33 @@
 
 
 
-// below is for the parameterized function 
+// // below is for the parameterized function 
+// // class Details {
+// //     String name;
+// //     String age;
+
+// //     public void show() {
+// //         System.out.println(this.name);
+// //         System.out.println(this.age);
+
+// //     }
+
+// //     Details(String n,String a) {
+         
+// //         this.name = n;
+// //         this.age = a;
+// //     }
+// // }
+
+// // public class classex {
+// //     public static void main(String[] args) {
+// //          Details ob1 = new Details("faisal", "12");
+// //          ob1.show();
+// //     }
+
+// // }
+
+// // below is for the copy constructor function 
 // class Details {
 //     String name;
 //     String age;
@@ -33,52 +60,94 @@
 
 //     }
 
-//     Details(String n,String a) {
-         
-//         this.name = n;
-//         this.age = a;
+//     Details( Details ob2) {
+//         this.name=ob2.name;
+//         this.age=ob2.age;
+        
+       
+//     }
+//     Details(){
+
 //     }
 // }
 
 // public class classex {
 //     public static void main(String[] args) {
-//          Details ob1 = new Details("faisal", "12");
-//          ob1.show();
+//          Details ob1 = new Details();
+//          ob1.name="faisal";
+//          ob1.age="112";
+//          Details ob2 = new Details(ob1);
+
+         
+//          ob2.show();
 //     }
 
 // }
 
-// below is for the copy constructor function 
-class Details {
+
+
+// class  Details{
+//     int age;
+//     String name;
+    
+// }
+
+/* 
+class Details{
     String name;
-    String age;
+    int age;
 
-    public void show() {
-        System.out.println(this.name);
-        System.out.println(this.age);
-
-    }
-
-    Details( Details ob2) {
-        this.name=ob2.name;
-        this.age=ob2.age;
-        
-       
-    }
-    Details(){
-
-    }
+public void show(){
+    System.out.println(this.name);
+    System.out.println(this.age);
+ 
 }
-
+}
 public class classex {
     public static void main(String[] args) {
-         Details ob1 = new Details();
-         ob1.name="faisal";
-         ob1.age="112";
-         Details ob2 = new Details(ob1);
-
-         
-         ob2.show();
+        Details ob1=new Details();
+        ob1.name="faisal";
+        ob1.age=2;
+        ob1.show();
     }
-
+    
 }
+*/
+class C 
+{
+    static int a;
+    static int b;
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println(a);
+        System.out.println(C.a);
+        System.out.println(b);
+        System.out.println(C.b);
+        test();
+        System.out.println(a);
+        System.out.println(C.a);
+        System.out.println(b);
+        System.out.println(C.b);
+
+
+
+    }
+    public static void test(){
+        int b=30;
+        System.out.println(a);
+        System.out.println(C.a);
+        b=50;
+        a=10;
+    }
+}
+
+
+
+
+
+
+
+
+
+
